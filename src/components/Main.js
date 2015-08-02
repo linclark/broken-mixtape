9f8701e34a79c718052e4ad7f9201d5edc0bfbce
Lin Clark <lin.w.clark@gmail.com>
2015-08-02 15:11:48 -0400

    starter component

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div className="main-container">
+        <nav role="navigation">
+          <div>
+            asdfasdfasdf
+          </div>
+        </nav>
+      </div>
+    )
+  }
+});
+
+module.exports = Main;
