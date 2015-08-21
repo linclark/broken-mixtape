97a40ef77932fba5b797f6c0ed2cbd089a45e881
Lin Clark <lin.w.clark@gmail.com>
2015-08-21 12:10:41 -0400

    make it possible to npm link modules

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ["", ".js", ".json", ".jsx"]
+    extensions: ["", ".js", ".json", ".jsx"],
+    fallback: path.join(__dirname, "node_modules")
+  },
+  resolveLoader: {
+    fallback: path.join(__dirname, "node_modules")
   }
 };
