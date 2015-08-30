88d1882d9293df5e96401305ff0b22d36fabcf25
Lin Clark <lin.w.clark@gmail.com>
2015-08-30 16:31:54 -0400

    Revert "setup test"
    This reverts commit db8dd03f7cc3bbc30394bda6cb5f39aebe8f6e25.

--- test/actions/game.spec.js
+++
@@ -1,7 +0,0 @@
-var expect = require("expect");
-
-describe('todo actions', () => {
-  it('addTodo should create ADD_TODO action', () => {
-    expect(true).toEqual(true);
-  });
-});
--- test/jsdomReact.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var ExecutionEnvironment = require("react/lib/ExecutionEnvironment");
-var jsdom = require("mocha-jsdom");
-
-module.exports = function jsdomReact() {
-  jsdom();
-  ExecutionEnvironment.canUseDOM = true;
-};
